1f0e3480d4e273095e04d5e900a5a70bfe2e4ad6
yakhub-01A <43907751+yakhub-01A@users.noreply.github.com>
2021-11-21 14:50:52 +0530

    build

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use("/api/feed", feed);
 
 if(process.env.NODE_ENV === 'production') {
    
-  app.use(express.static(path.join(__dirname, "client", "build")))
+  app.use(express.static('client/build'));
 
   app.get('*',(req, res) => {
       res.sendFile(path.join(__dirname,'client','build','index.html'));
